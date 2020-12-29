71e231cb53c72f04ac75b10428fec4b91713be44
Albert Chang <albertchanged@gmail.com>
2020-12-28 17:54:28 -0800

    Utilize useEffect hook to store/update cart data in localStorage - Create InitializeCartAction that passes cart as payload - Create 2 useEffect hooks: 	1. To get stringified cart from localStorage and set as cart object in state (only on page load) 	2. To stringify the updated cart data to store in localStorage (whenever cart data changes)

--- src/components/AppState.tsx
+++ src/components/AppState.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, useContext, useReducer} from "react";
+import React, {createContext, useContext, useEffect, useReducer} from "react";
 
 interface CartItem {
   id: number;
@@ -43,7 +43,16 @@ interface AddToCartAction extends Action<"ADD_TO_CART"> {
   }
 }
 
-const reducer = (state: AppStateValue, action: AddToCartAction) => {
+interface InitializeCartAction extends Action<"INITIALIZE_CART"> {
+  payload: {
+    cart: AppStateValue["cart"];
+  }
+}
+
+const reducer = (
+  state: AppStateValue,
+  action: AddToCartAction | InitializeCartAction
+) => {
   if (action.type === "ADD_TO_CART") {
     const itemToAdd = action.payload.item;
     const itemExists = state.cart.items.find((item) => item.id === itemToAdd.id);
@@ -72,6 +81,11 @@ const reducer = (state: AppStateValue, action: AddToCartAction) => {
             ]
       }
     }
+  } else if (action.type === "INITIALIZE_CART") {
+    return {
+      ...state,
+      cart: action.payload.cart
+    }
   }
 
   return state;
@@ -80,6 +94,23 @@ const reducer = (state: AppStateValue, action: AddToCartAction) => {
 const AppStateProvider: React.FC = ({children}) => {
   // Ensure state always has properties specified in AppStateValue
   const [state, dispatch] = useReducer(reducer, defaultStateValue)
+
+  useEffect(() => {
+    const cart = window.localStorage.getItem("cart");
+    if (cart) {
+      dispatch({
+        type: "INITIALIZE_CART",
+        payload: {
+          cart: JSON.parse(cart)
+        }
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    // Store/update cart in localStorage if cart changes
+    window.localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <AppStateContext.Provider value={state}>
